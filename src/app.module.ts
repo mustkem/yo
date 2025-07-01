e8ad2877c9016808a4c717329657f2f0ef6a499f
mustkem <mo.mustkem@gmail.com>
2025-07-01 12:18:55 +0530

    feat: use config module for dynamic env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdDbModule } from './commons/db.module';
+import { DatabaseModule } from './commons/db.module';
 import { ApiModule } from './api.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ApiModule, ProdDbModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
