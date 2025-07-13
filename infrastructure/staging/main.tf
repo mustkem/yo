793e09c2c0393e769695ae8a366f236201e51db3
mustkem <mo.mustkem@gmail.com>
2025-07-13 19:57:03 +0530

    provision network acl configurations using terraform

--- infrastructure/staging/main.tf
+++ infrastructure/staging/main.tf
@@ -236,17 +236,3 @@ resource "aws_instance" "staging_server" {
   }
 }
 
-# -------------------------
-# OUTPUTS
-# -------------------------
-output "public_ip" {
-  value = aws_instance.staging_server.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.staging_server.id
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.nestjs_app.repository_url
-}
