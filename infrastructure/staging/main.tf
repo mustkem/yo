b491a9742f5f634e0438483d23224df5a302616c
mustkem <mo.mustkem@gmail.com>
2025-07-23 07:46:28 +0530

    update ec2 instance type

--- infrastructure/staging/main.tf
+++ infrastructure/staging/main.tf
@@ -61,6 +61,7 @@ resource "aws_subnet" "staging_subnet" {
   vpc_id                  = aws_vpc.staging_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name        = "staging-subnet"
